8fde52274eddcf35c56e0261ebbd0f301d04cb92
Mohamed Sameh <mswebfactories@gmail.com>
2016-10-01 17:30:45 +0000

    Added username column, user profile pages, and username sign up form.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,7 +11,13 @@ class PagesController < ApplicationController
 
   # back-end code for pages/profile
   def profile
+    if(User.find_by_username(params[:id]))
+      @username = params[:id]
+    else
+      redirect_to root_path, :notice=> "User not found!"
+    end
   end
+  
 
   # back-end code for pages/explore
   def explore
